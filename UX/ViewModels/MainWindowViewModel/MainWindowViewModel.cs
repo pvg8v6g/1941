17fece8f362590ea57d27d82d5ab2179756ddf8a
philbusiness52@gmail.com <philbusiness52@gmail.com>
2025-11-23 23:55:45 -0600

    textures

--- UX/ViewModels/MainWindowViewModel/MainWindowViewModel.cs
+++ UX/ViewModels/MainWindowViewModel/MainWindowViewModel.cs
@@ -2,6 +2,8 @@
 
 public class MainWindowViewModel : BaseViewModel.BaseViewModel
 {
+    public string WorldWaterImagePath => "/Graphics/Images/world_water.svg";
+    public string WorldImagePath => "/Graphics/Images/world.svg";
 
     #region Actions
 
